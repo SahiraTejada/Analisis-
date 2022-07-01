2313735e7ffcbe58e02bc1ae1145c519a2fb1d1b
SahiraTejada <81046432+SahiraTejada@users.noreply.github.com>
2022-06-30 22:03:54 -0400

    Add files via upload

---
+++ Analysis/Analysis/Exercise.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Analysis
+{
+    public class Exercise
+    {
+
+        /*
+        public static string Greeting()
+        {
+            return "Hello you";
+        }
+
+        public static string Greeting(string name)
+        {
+            return "Hello" + name + "!";
+        }*/
+
+        public static string Greeting(string name = null)
+        {
+            if (name == null)
+            {
+                return "Hello you";
+            }
+            return "Hello" + name + "!";
+        }
+    }
+}
---
+++ Analysis/Analysis/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Analysis
+{
+    class Program
+    {
+        static void Main()
+        {
+            var implemetationFilePath = @"C:\Users\HP\source\repos\Analysis\Analysis\Exercise.cs";
+            var impletation  = File.ReadAllText(implemetationFilePath);
+            var tree = CSharpSyntaxTree.ParseText(impletation);
+            var root  = tree.GetRoot();
+
+            if (UsesMethodOverloading(root))
+            {
+              Console.WriteLine("Hello World!");
+            }
+
+            if (UsesParametersOverloading(root))
+            {
+                throw new Exception("PARAMETROS");
+            }
+
+
+
+
+
+        }
+
+        private static bool UsesMethodOverloading(SyntaxNode root)
+        {
+            return root.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Count(method => method.Identifier.Text == "Greeting") > 1;
+        }
+
+
+        private static bool UsesParametersOverloading(SyntaxNode root)
+        {
+            var gretting =  root.DescendantNodes()
+                .OfType<MethodDeclarationSyntax>()
+                .Single(method => method.Identifier.Text == "Greeting");
+
+           return gretting.ParameterList.Parameters[0].Default.Value is LiteralExpressionSyntax literal &&
+                literal.IsKind(SyntaxKind.NullLiteralExpression);
+        }
+    }
+}
